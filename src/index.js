0baaca402fbaa7110074d81562f0ea4b6d02c5bc
GabrielFelix23 <srgfelix123@gmail.com>
2020-08-14 17:13:23 -0300

    Instalando Expo.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+import Styles from './public/styles'
+
+export default function App() {
+  return (
+    <View style={Styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
